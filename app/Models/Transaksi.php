727e83706622259b0f629bf0360053efb91599e2
Ahmad Yassin <ahmadyassin4521@gmail.com>
2025-06-02 21:16:14 +0700

    hh

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -5,12 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
 class Transaksi extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'nama_penyewa','nama_barang','barang_id', 'jumlah_sewa', 'durasi_sewa', 'total_harga', 'status'
+        'order_id',         // <-- TAMBAHKAN INI!
+        'nama_penyewa',
+        'nama_barang',
+        'barang_id',
+        'jumlah_sewa',
+        'durasi_sewa',
+        'total_harga',
+        'status'
+        // pastikan semua kolom yang ingin Anda isi melalui create() atau update() ada di sini
     ];
 }
